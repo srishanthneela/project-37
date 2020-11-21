fa6f4b794a99f9b897947fff431786f77c5b024d
srishanthneela <72262599+srishanthneela@users.noreply.github.com>
2020-11-21 10:21:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,9 +16,9 @@ function preload(){
 
   cloudImage = loadImage("cloud.png");
 
-  trex_running=loadAnimation("images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg",
- "images/6.jpg","images/7.jpg","images/8.jpg","images/9.jpg","images/10.jpg","images/11.jpg","images/12.jpg");
- trex_collided=loadImage("images/1.jpg");
+  trex_running=loadAnimation("1.jpg","2.jpg","3.jpg","4.jpg","5.jpg",
+ "6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg");
+ trex_collided=loadImage("11.jpg");
   groundImage = loadImage("ground2.png");
   
   obstacle1 = loadImage("obstacle1.png");
@@ -149,7 +149,7 @@ function spawnClouds() {
     cloud.velocityX = -3;
     
      //assign lifetime to the variable
-    cloud.lifetime = 300;
+    cloud.lifetime = 500;
     
     //adjust the depth
     cloud.depth = trex.depth;
